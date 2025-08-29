67830c9d12bbab133c3b6d3f8b3881831f5dc8b0
Israel Castañeda <137331654+IsrC11@users.noreply.github.com>
2025-08-29 17:44:46 -0600

    Delete maya_chem/__init__.py

--- maya_chem/__init__.py
+++
@@ -1,6 +0,0 @@
-from .analyzer import MayaAnalyzer
-from .config import MayaConfig
-from .utils import compute_activity_metrics
-
-__all__ = ['MayaAnalyzer', 'MayaConfig', 'compute_activity_metrics']
-__version__ = '0.1.0'

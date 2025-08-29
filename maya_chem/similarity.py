f6dbe6f47ac5f38493e4a623a3aa920896570b06
Israel Castañeda <137331654+IsrC11@users.noreply.github.com>
2025-08-29 13:02:37 -0600

    Update similarity.py

--- maya_chem/similarity.py
+++ maya_chem/similarity.py
@@ -18,6 +18,9 @@ def compute_similarity_matrix(fps: List, n_jobs: int) -> np.ndarray:
     def compute_sim(fp, fps_list):
         return [round(DataStructs.TanimotoSimilarity(fp, f), 3) for f in fps_list]
 
-    sim_matrix = np.array(Parallel(n_jobs=n_jobs)(delayed(compute_sim)(fp, fps) for fp in fps))
-    np.fill_diagonal(sim_matrix, 1.0)
+    sim_matrix = np.zeros((len(fps), len(fps)))
+    for i range(len(fps)):
+        sims = DataStructs.BulkTanimotoSimmilarity(fps[i], fps[i:])
+        sim_matrix[i, i:] =　sims
+        sim_matrix[i:, i] = sims
     return sim_matrix

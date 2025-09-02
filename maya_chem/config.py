2f42721d3902c10ddb70f355d28b2722afc5ae08
Israel Castañeda <137331654+IsrC11@users.noreply.github.com>
2025-09-02 16:35:54 -0600

    Update config.py

--- maya_chem/config.py
+++ maya_chem/config.py
@@ -21,14 +21,11 @@ class MayaConfig:
             'smiles_col': None,
             'activities': [],
             'eval_metric': None,
-            'metric_value': None
-        }
+            'metric_value': None}
 
         self.analysis = {
             'descriptors': [],
-            'reduction_method':'pca'
-        }
+            'reduction_method':'pca'}
 
         self.viz = {
-            'output_dir':'./results'
-        }
+            'output_dir':'./results'}

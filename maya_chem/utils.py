816f142726b28b965420a05ab1c15fe42392f8b8
Israel Castañeda <137331654+IsrC11@users.noreply.github.com>
2025-09-02 16:58:05 -0600

    Update utils.py

--- maya_chem/utils.py
+++ maya_chem/utils.py
@@ -25,6 +25,9 @@ def load_data(file_path: str, id_col: str = "ID", smiles_col: str = "SMILES") ->
         df = pd.read_csv(file_path)
     elif ext in [".tsv", ".txt"]:
         df = pd.read_csv(file_path, sep="\t")
+
+    elif ext in ['.xls', 'xlsx']
+        df = pd.read_excel(file_path)
     else:
         raise ValueError(f"Unsupported file extension: {ext}")
 

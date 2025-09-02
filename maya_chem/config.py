b3c500509dfa0d0ccd0a15c9077abb4904f101e3
Israel Castañeda <137331654+IsrC11@users.noreply.github.com>
2025-09-02 16:42:26 -0600

    Update config.py

--- maya_chem/config.py
+++ maya_chem/config.py
@@ -8,16 +8,8 @@ class MayaConfig:
         
         self.data_path = data_path
         
-        self.data = {
-            'id_col': None,
-            'smiles_col': None,
-            'activities': [],
-            'eval_metric': None,
-            'metric_value': None}
+        self.data = {'id_col': None, 'smiles_col': None, 'activities': [], 'eval_metric': None, 'metric_value': None}
 
-        self.analysis = {
-            'descriptors': [],
-            'reduction_method': 'pca'}
+        self.analysis = {'descriptors': [], 'reduction_method': 'pca'}
 
-        self.viz = {
-            'output_dir': './results'}
+        self.viz = {'output_dir': './results'}

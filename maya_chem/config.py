d21d6bf4e538d7ecbe64ceae928e500e24926a5f
Israel Castañeda <137331654+IsrC11@users.noreply.github.com>
2025-09-02 16:50:23 -0600

    Update config.py

--- maya_chem/config.py
+++ maya_chem/config.py
@@ -5,11 +5,7 @@ from dataclasses import dataclass
 @dataclass
 class MayaConfig:
     def __init__(self, data_path: str = None):
-        
         self.data_path = data_path
-        
         self.data = {'id_col': None, 'smiles_col': None, 'activities': [], 'eval_metric': None, 'metric_value': None}
-
         self.analysis = {'descriptors': [], 'reduction_method': 'pca'}
-
         self.viz = {'output_dir': './results'}

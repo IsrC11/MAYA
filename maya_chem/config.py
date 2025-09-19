f5566d326ecee83845ea6810ccff5254ad1ad304
Israel Castañeda <137331654+IsrC11@users.noreply.github.com>
2025-09-19 11:44:04 -0600

    Update config.py

--- maya_chem/config.py
+++ maya_chem/config.py
@@ -7,5 +7,5 @@ class MayaConfig:
     def __init__(self, data_path: str = None, output_dir: str = './results', fingerprint: str | list = 'morgan', reduction_method: str | list = 'pca', properties: list = None):
         self.data_path = data_path
         self.data = {'id_col': None, 'smiles_col': None, 'activities': [], 'eval_metric': None, 'metric_value': None}
-        self.analysis = {'fingerprint': fingerprint, 'reuction_method': reduction_method, 'properties': properties or ['MolWt', 'LogP', 'HBA', 'HBD', 'TPSA']}
+        self.analysis = {'fingerprint': fingerprint, 'reduction_method': reduction_method, 'properties': properties or ['MolWt', 'LogP', 'HBA', 'HBD', 'TPSA']}
         self.viz = {'output_dir': output_dir}

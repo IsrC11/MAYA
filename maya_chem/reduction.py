b89bf3464c38815e2b26a4c97ed27828ea8a6656
Israel Castañeda <137331654+IsrC11@users.noreply.github.com>
2025-09-19 11:48:14 -0600

    Update reduction.py

--- maya_chem/reduction.py
+++ maya_chem/reduction.py
@@ -2,15 +2,21 @@
 import pandas as pd
 from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
+import umap
 
 def apply_pca(fps, n_components: int = 2) -> pd.DataFrame:
     """Apply PCA dimensionality reduction."""
     pca = PCA(n_components=n_components)
     coords = pca.fit_transform(fps)
-    return pd.DataFrame(coords, columns=[f"PC{i+1}" for i in range(n_components)])
+    return pd.DataFrame(coords, columns=[f"PCA{i+1}" for i in range(n_components)])
 
 def apply_tsne(fps, n_components: int = 2, perplexity: int = 30) -> pd.DataFrame:
     """Apply t-SNE dimensionality reduction."""
     tsne = TSNE(n_components=n_components, perplexity=perplexity, random_state=42)
     coords = tsne.fit_transform(fps)
-    return pd.DataFrame(coords, columns=[f"TSNE{i+1}" for i in range(n_components)])
+    return pd.DataFrame(coords, columns=[f"Dimension{i+1}" for i in range(n_components)])
+
+def apply_umap (fps, n_components=2):
+    reducer = umap.UMAP(n_components=n_components, random_state=42)
+    coords = reducer.fit_transforms(fps)
+    return pd.DataFrame(coords, columns=[f'Dimension{i+1}' for i in range(n_components)])

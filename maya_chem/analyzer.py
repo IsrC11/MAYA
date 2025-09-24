6447c94592b0ea282af276434df09c8dbe1363f8
Israel Castañeda <137331654+IsrC11@users.noreply.github.com>
2025-09-24 17:17:39 -0600

    Update analyzer.py

--- maya_chem/analyzer.py
+++ maya_chem/analyzer.py
@@ -61,6 +61,7 @@ class MayaAnalyzer:
         from google.colab.output import serve_kernel_port_as_iframe
         from .visualization import plot_similarity_heatmap
         from dash import Dash, dcc, html, Input, Output
+        from jupyter_dash import jupyterDash
         
         if len(coords_cols) < 2:
             raise ValueError('At least two reduction columns (PC1/PC2 or Dim1/Dim2) were not found')
@@ -82,7 +83,7 @@ class MayaAnalyzer:
                 fig= px.scatter(self.data, x=x_col, y=y_col, color='MolWt' if 'MolWt'in self.data.columns else None, title=title, width=1200, height=900)
                 fig = molplotly.add_molecules(fig=fig, df=self.data, smiles_col=self.config.data['smiles_col'], title_col=self.config.data['id_col'], color_col='MolWt' if 'MolWt' in self.data.columns else None)
 
-                app = Dash(__name__)
+                app = JupyterDash(__name__)
                 options = ([{'label': f'{fp}', 'value': fp} for fp in self.data['Fingerprint'].unique()] if 'Fingerprint' in self.data.columns else [{'label': 'Default', 'value':'default'}])
                 app.layout = html.Div([html.H3('Chemical Space Interactive Viewer'), dcc.Dropdown(id='combo-selector', options=options, value=options[0]['value'], clearable=False), dcc.Graph(id='scatter-plot', figure=fig),])
                 @app.callback(Output('scatter-plot', 'figure'), Input('combo-selector', 'value'))
@@ -95,8 +96,8 @@ class MayaAnalyzer:
                     fig = molplotly.add_molecules(fig=fig, df=df_filtered, smiles_col=self.config.data['smiles_col'], title_col=self.config.data['id_col'], color_col = 'MolWt' if 'MolWt'in self.data.columns else None)
                     return fig
 
-                serve_kernel_port_as_iframe('localhost', 8050)
-                app.run(port=8050)
+                #serve_kernel_port_as_iframe('localhost', 8050)
+                app.run_server('inline', port=8050)
                 return app
                 
             except Exception as e:

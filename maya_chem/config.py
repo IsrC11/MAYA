2ffce2755fba383aca1f07d931c7eeb871072d1a
Israel Castañeda <137331654+IsrC11@users.noreply.github.com>
2025-09-25 14:46:39 -0600

    Update config.py

--- maya_chem/config.py
+++ maya_chem/config.py
@@ -4,8 +4,8 @@ from dataclasses import dataclass
 
 @dataclass
 class MayaConfig:
-    def __init__(self, data_path: str = None, output_dir: str = './results', fingerprint: str | list = 'morgan', reduction_method: str | list = 'pca', properties: list = None):
+    def __init__(self, data_path: str = None, output_dir: str = './results', fingerprint: str | list = 'morgan', reduction_method: str | list = 'pca', properties: list = None, color_by: str = 'LogP', palette: str = 'viridis'):
         self.data_path = data_path
         self.data = {'id_col': None, 'smiles_col': None, 'activities': [], 'eval_metric': None, 'metric_value': None}
         self.analysis = {'fingerprint': fingerprint, 'reduction_method': reduction_method, 'properties': properties or ['MolWt', 'LogP', 'HBA', 'HBD', 'TPSA']}
-        self.viz = {'output_dir': output_dir}
+        self.viz = {'output_dir': output_dir, 'color_by':color_by, 'palette':palette}

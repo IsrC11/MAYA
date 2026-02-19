9d884c266c557fcc3fcbe0f7ab362c7ccfde2f58
Israel Castañeda <137331654+IsrC11@users.noreply.github.com>
2026-02-18 20:51:13 -0600

    Update config.py

--- maya_chem/config.py
+++ maya_chem/config.py
@@ -7,5 +7,6 @@ class MayaConfig:
     def __init__(self, data_path: str = None, output_dir: str = './results', fingerprint: str | list = 'morgan', reduction_method: str | list = 'pca', properties: list = None, color_by: str = 'LogP', palette: str = 'RdBu_r'):
         self.data_path = data_path
         self.data = {'id_col': None, 'smiles_col': None, 'activities': [], 'eval_metric': None, 'metric_value': None}
+        sel.curation = {"standardize": True, "largest_fragment": True, "neutralize": False, "canonical_tautomer": False}
         self.analysis = {'fingerprint': fingerprint, 'reduction_method': reduction_method, 'properties': properties or ['MolWt', 'LogP', 'HBA', 'HBD', 'TPSA']}
         self.viz = {'output_dir': output_dir, 'color_by':color_by, 'palette':palette}
